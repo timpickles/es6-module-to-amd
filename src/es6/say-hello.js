3356f0981fd742b0179edeae8e000ab30182914f
Tim Pickles <timpickles@gmail.com>
2015-02-11 18:42:05 +0000

    Made h1 immutable

--- src/es6/say-hello.js
+++ src/es6/say-hello.js
@@ -1,6 +1,6 @@
 export class Hello {
     static render() {
-        let h1 = document.createElement('h1');
+        const h1 = document.createElement('h1');
         h1.textContent = "Hello I'm ES6.";
 
         document.body.appendChild(h1);
